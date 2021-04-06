84e1cf59366c3bd64f89c7051d52b962ea3124fd
abc04 <81609152+abc04@users.noreply.github.com>
2021-04-06 14:55:27 +0900

    한글 홈페이지 주소 수정

---
+++ text_kr.php
@@ -0,0 +1,199 @@
+<?php
+define('slide_title',      "Coding 교육을 위한 소프트웨어 연구소입니다.
+책상이 아닌, 현장에서 연구합니다.
+SW교육의 대중화와, 인식개선을 위해 노력합니다.
+수요자 중심의 의미있는 SW교육을 꿈꿉니다.");
+define('slide_sub', "만지고 만들면서 쉽게 배우는<br />글로벌코딩");
+define('slide_shop_title1','생활코딩
+코D알고 교재와 15차시 온라인 강의');
+define('slide_shop_sub1','아두이노와 지능형 센서를 활용하여 생활 주변 기기를 만드는 텍스트 기반 IOT 코딩');
+define('slide_shop_title2','미술코딩
+15차시 온라인 강의');
+define('slide_shop_sub2','아두이노와 지능형 센서를 활용하여 미술작품을 만들면서 배우는 텍스트 기반 IOT 코딩');
+define("top_menu1","강의");
+define("top_menu2","커뮤니티");
+define("top_menu3","북마크");
+define("top_menu4","로그아웃");
+define("top_menu5","마이페이지");
+define("top_menu6","회원가입");
+define("top_menu7","로그인");
+define("menu1","회사소개");
+define("menu2","코딩이란");
+define("menu3","커뮤니티");
+define("menu4","공지사항");
+define("menu5","글로벌소식");
+define("menu6","자료실");
+define("menu7","1:1 문의");
+//GCR fann_test
+define("ceo", "대표");
+define("ceo_name", "이창훈");
+define("company_number", "사업자등록번호");
+define("addr1","주소");
+define("addr1_name", "51734 경남 창원시 마산합포구 3.15대로 298 글로벌코딩연구소 (서성동, 경남대학교 창업보육관 406)");
+
+define("page_title_coding","코딩이란");
+define("page_title_company","회사소개");
+define("page_title_community","커뮤니티");
+define("page_title_lecture","온라인 강의");
+
+define("ment1","<p>코딩을 배우는 것은 여러분의 미래는 물론 <br>조국의 미래에도 매우 중요합니다.</p>
+<h5>버락 오바마 前 미국 대통령</h5>");
+define("ment2","  <p>모든 사람은 코딩을 배워야 한다. <br>코딩은 생각하는 방법을 가르쳐준다.</p>
+<h5>故 스티브 잡스 애플 C.E.O </h5>");
+define("ment3","  <p>자동차는 이제 가솔린이 아니라 <br>소프트웨어로 움직인다.</p>
+<h5>메르세데스 前 벤츠 회장</h5>");
+define("title1","미래 소프트웨어 교육을 선도하는 전문 연구기관");
+define("content1"," <h4>오직 코딩 교육을 위한</h4><br />
+<p>Coding 교육을 위한 소프트웨어 연구소입니다.</p>");
+define("content2","<h4>현장 중심 체험</h4>
+<p>책상이 아닌, 현장에서 연구합니다.</p");
+define("content3"," <h4>SW대중화에 기여</h4>
+<p>SW교육의 대중화와, 인식개선을 위해 노력합니다.</p>");
+define("content4"," <h4>의미있는 SW교육</h4>
+<p>수요자 중심의 의미있는 SW교육을 꿈꿉니다.</p>");
+define("company_title1","  <h2>글로벌코딩 연구소</h2>
+<p>
+글로벌코딩연구소 는 좀 더 의미있는 소프트웨어 교육을 받을 수 있는 환경을 만들고자 노력하는 소프트웨어 교육 전문가 집단입니다. 따라하기 식의 주입식에서 벗어나 자신이 상상한 것을 직접 표현할 수 있는 창의을 지향합니다.
+</p>");
+define("company_title2","<h3>'16년 미래부 선정 K-Global 300기업</h3>
+<p>
+K-Global 300이란 글로벌 진출 가능성과 성장 잠재력을 갖춘 유망 ICT 창업·벤처기업을 대상으로 “선택과 집중” 원칙에 따라 미래부의 정책지원 역량을 결집하여 지원하기 위해 선정한 기업입니다.
+</p>");
+define("company_purpose_title","설립 취지");
+define("company_purpose_content1","미래 학자들은 미래 국가 경쟁력의 기초가 소프트웨어 산업이 될 것이라고 예측하고 있습니다. <br>
+이러한 시대적 흐름에 맞추어 세계의 많은 국가들이 프로그래밍 교육에 주목하고 있습니다. <br>
+그들이 주장하는 프로그래밍 교육의 핵심은 ‘대중화’입니다. 좀 더 많은 학생들이 좀 더 이른 나이부터 프로그래밍을 접하도록 하고자 하는 것입니다. <br>
+미국과 핀란드, 영국에서는 이미 공교육의 영역에서 이러한 교육이 실시되고 있습니다. <br><br>
+
+우리나라에서는 정부와 대기업, 민간단체를 중심으로 초‧중‧고등학생들을 위한 프로그래밍 교육이 일부 실시되고 있으나 아직 미약한 실정입니다.<br>
+충분한 준비 없이 개발자의 시각으로만 준비된 많은 교육 사업들이 단편적인 보여주기 식 행사에 그치거나 무조건 따라하기 식의 일방향적 강의에 머무르고 있습니다.");
+define("company_purpose_content2","이러한 방식으로는 학생들에게 유의미한 학습이 일어날 수 없습니다. <br><br>
+
+글로벌 코딩 연구소는 이런 문제의식을 바탕으로 시작되었습니다. 소프트웨어 교육은 개발자의 관점이 아닌 교육자의 관점에서 시작되어야 합니다. <br>
+우리 아이들이 ‘자신의 시각과 사고’로 소프트웨어를 배워나가는 과정을 통해 논리력과 창의력, 문제해결능력을 증진시킬 수 있습니다. <br>
+소프트웨어 교육 연구소는 ‘유의미한 있는 소프트웨어 교육’이 이루어질 수 있도록 고민하고 해답을 찾아가고자 합니다. <br>
+그리고 그 해답을 통해 더 많은 우리 아이들이 소프트웨어를 배울 수 있도록 하려고 합니다.");
+define("company_main_business",'<h3 class="align-center">주요사업</h3>
+<ul class="g-list g-clear">
+<li>
+소프트웨어 교육 연구
+<ul class="g-list g-clear" style="margin-left:0px;padding-left:0px">
+<li>기초교육 연구, 교육과정 개발,  교육 컨텐츠 개발, SW 교육 커뮤니티 활성화 지원, 소프트웨어 교육 실천</li>
+<li>학생교육, 학부모교육, 교사/강사 교육, SW교육봉사</li>
+</ul>
+</li>
+
+</ul>');
+define("company_main_value",'<h3 class="align-center">핵심가치</h3>
+<ul class="g-list g-clear">
+<li>학생중심의 자기주도적 학습과 성장</li>
+<li>소프트웨어 교육의 전문성</li>
+<li>공유와 개방을 통한 대중화</li>
+<li>책상의 자리가 아닌 교육 현장에서의 연구</li>
+<li>사회적 책임 실천을 통한 교육 기회의 확대</li>
+</ul>');
+define("company_summary_title",'회사개요');
+define("company_summary_table",' <table class="g-table">
+<caption class="screen-hide">회사개요 테이블</caption>
+<col style="width: 30%;">
+<col style="width: *;">
+<tbody>
+<tr>
+<th>회사명</th>
+<td>
+글로벌코딩 연구소
+</td>
+</tr>
+<tr>
+<th>대표자</th>
+<td>
+이창훈
+</td>
+</tr>
+<tr>
+<th>소재지</th>
+<td>
+​경남 창원시 마산합포구 3.15대로 298 경남대학교 <br>창업보육관 406 (우) 51734
+</td>
+</tr>
+<tr>
+<th>이메일</th>
+<td>
+hoo7987@hanmail.net
+</td>
+</tr>
+
+</tbody>
+</table>');
+define("coding_about",'<h2>코딩이란</h2>
+<p>
+컴퓨터 언어를 사용해 프로그램을 만들어 가는 과정을 말합니다. 우리는 코딩이라는 말보다 프로그래밍이라는 말을 많이 썼습니다.
+사실은 우리 주변에 있는 대부분 가전제품이나 기계제품에도 다 코딩이 들어가게 됩니다.
+예를 들면 엘리베이터를 타고 나서 버튼을 누르면 그 해당 층으로 이동하게 하는 것도 코딩이 들어가고 자판기에 돈을 넣고 버튼을 눌렸을 때 음료수가 나오는 것에도 코딩이 들어가게됩니다.
+</p>');
+define("coding_apply_case",'  <h2>코딩 적용사례</h2>
+<p>
+코딩은 어떻게 보면 컴퓨터에게 대화를 시도하는 하나의 언어, 도구, 수단이라고 볼 수 있습니다. <br>
+컴퓨터가 알아들을 수 있는 형태로 언어를 작성하는 것입니다.
+</p>');
+define("coding_content1",'<h5>우리 주변 가전제품</h5>
+<p>우리 주변 가전제품, 기계제품에도 코딩이 이용됩니다.</p>');
+define("coding_content2",'<h5>엘리베이터</h5>
+<p>엘리베이터 버튼을 눌러 해당 층으로 이동시에도 코딩을 이용합니다.</p>');
+define("coding_content3",'<h5>자판기 작동</h5>
+<p>자판기에 돈을 넣고 버튼을 눌렀을 때 음료수가 나오는것에도 코딩이 들어갑니다.</p>');
+define("coding_importance",'코딩의 중요성');
+define("coding_begin",'<h2>코딩 교육의 시작</h2>
+<p>
+영국은 2014년부터 학교에서 코딩 교육을 하기 시작했고, 이를  시작으로 세계 각국에서 코딩 교육 열풍이 불고 있습니다.
+한국 또한 2017년부터 초등학교와 중학교에서 소프트웨어 교육이 의무화되고, 고등학교의 경우 2018년부터 해당 과목이 심화선택  과목에서 일반선택 과목으로 바뀌게 됩니다. 이처럼 미래엔 코딩의 중요성이 강조되는 사회가 되겠죠?
+</p>');
+define("coding_necessary",'필수가 되고있는 코딩 교육');
+define("coding_necessary_content1",'<h5>컴퓨팅(소프트웨어) 필수과목 도입</h5>
+<p>영국은 지난 2014년 9월부터 컴퓨팅 과목을 만 5~16세의 모든 학년에 필수과목으로 지정.</p>');
+define("coding_necessary_content2",'    <h5>미국, 프랑스, 핀란드, 에스토니아, 이스라엘 </h5>
+    <p>각국에서 SW 과목(프로그래밍도 배우고 간단한 어플리케이션을 만드는 내용)을 활발히 교육과정에 포함.</p>');
+define("coding_necessary_content3",'<h5>세계 각각의 변화</h5>
+<p>세계 각국은 정보화 시대에 들어오면서부터 소프트웨어 교육을 활발히 진행하고 있습니다.</p>');
+define("coding_program_title",'프로그램 구성');
+define("coding_program_content",'<li><div class="h5" style="margin: 0;">초,중 방과후 학교 코딩 교실 </div></li>
+<li><div class="h5" style="margin: 0;">중등 자유학기제(학년제) 코딩교실 </div></li>
+<li><div class="h5" style="margin: 0;">코딩(소프트웨어교육) 강사과정 </div></li>
+<li><div class="h5" style="margin: 0;">코딩(소프트웨어교육) 교육자료 공급 </div></li>');
+define("coding_program_content2",'  <li>
+      <div class="h5 color-spot" style="margin: 0;">방과후 학교 코딩 교실</div>
+      <div>주 1회 (80분, 수강료: 35,000원) </div>
+  </li>
+  <li>
+      <div class="h5 color-spot" style="margin: 0;">중등 자유학기제(학년제) 코딩교실</div>
+      <div>주 1회 (80분, 수강료: 35,000원) </div>
+  </li>
+  <li>
+      <div class="h5 color-spot" style="margin: 0;">코딩(소프트웨어교육) 강사과정</div>
+      <ul class="g-list">
+          <li>코딩강사과정 : 50시간 </li>
+          <li>코딩강사코칭과정 120시간 </li>
+          <li>강사과정수료후 강사자격증 취득 </li>
+          <li>자유학기제, 방과후 강사 추천 </li>
+
+      </ul>
+  </li>');
+  define("bi_company_name",'주식회사 글로벌코딩연구소');
+
+  define("bi_company_addr1",'주소');
+
+  define("bi_cs_email",'이메일: ');
+
+  define("bi_company_introduction", "회사소개");
+
+  define("bi_company_terms", "이용약관");
+
+ define("bi_company_Privacy",'개인정보처리방침');
+
+define("bi_company_noemail", "이메일무단수집거부");
+
+define("bi_map", "상세지도");
+
+define("bi_mobile", "모바일버전");
+
+?>
